e6d11e02542129f2093394ac482a8072a784bdba
Jeel Patel <98872743+Jeelpatel192@users.noreply.github.com>
2023-02-23 14:31:59 +0530

    Add files via upload

---
+++ implement_interface.java
@@ -0,0 +1,25 @@
+// Program 6 : Write a java program to implement interface.
+
+interface Printable{  
+    void print();  
+}  
+
+interface Showable{  
+    void show();  
+}  
+
+public class A7 implements Printable,Showable {  
+    public void print(){
+        System.out.println("Hello");
+    }  
+    public void show(){
+        System.out.println("Welcome");
+    
+    }  
+  
+    public static void main(String args[]){  
+        A7 obj = new A7();  
+        obj.print();  
+        obj.show();  
+    }  
+}
