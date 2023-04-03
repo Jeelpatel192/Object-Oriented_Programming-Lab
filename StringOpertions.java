75d19d2e96ef9dca4bb79f67c3fba2f2ef6a0d8c
Jeel Patel <98872743+Jeelpatel192@users.noreply.github.com>
2023-04-03 09:26:05 +0530

    Update StringOpertions.java

--- StringOpertions.java
+++ StringOpertions.java
@@ -1,4 +1,4 @@
-public class StringOpertions {
+public class StringExp {
     public static void main(String args[]) {
         String s1 = "Hello";
         String s2 = "World!";
@@ -15,7 +15,7 @@ public class StringOpertions {
         Boolean b1 = s1.equals(s2);
         System.out.println("Is S1 and S2 equal : " + b1);
         
-        String s3 = "Hello friends\"chai pilo";
+        String s3 = "Hello friends\"chai pilo\".";
         System.out.println(s3);
     }
 }
